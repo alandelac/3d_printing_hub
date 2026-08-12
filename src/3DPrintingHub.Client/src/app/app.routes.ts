b3bbfdad7a004ab2e146c4ee4770b2d0aea2517f
Alan de la Cruz <alan09delacruz@gmail.com>
2026-08-11 22:23:31 -0600

    feat: add stocked page with route and navigation link
    Added a new StockedPageComponent (with HTML and CSS files) and registered it in the app routes under `/stocked`. Also added a corresponding navigation link in the navbar.

--- src/3DPrintingHub.Client/src/app/app.routes.ts
+++ src/3DPrintingHub.Client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { FilamentsPageComponent } from './features/filaments/pages/filaments-page.component';
 import { ModelsPageComponent } from './features/models/pages/models-page.component';
 import { SettingsPageComponent } from './features/settings/pages/settings-page.component';
+import { StockedPageComponent } from './features/stocked/pages/stocked-page.component';
 
 export const routes: Routes = [
   { path: 'filaments', component: FilamentsPageComponent },
   { path: 'models', component: ModelsPageComponent },
   { path: 'settings', component: SettingsPageComponent },
+  { path: 'stocked', component: StockedPageComponent },
   { path: '', redirectTo: 'filaments', pathMatch: 'full' }
 ];
 
---
+++ src/3DPrintingHub.Client/src/app/features/stocked/pages/stocked-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stocked-page',
+  standalone: true,
+  templateUrl: './stocked-page.component.html',
+  styleUrls: ['./stocked-page.component.css']
+})
+export class StockedPageComponent {}
